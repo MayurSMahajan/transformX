cb68fb67d9f7da5b3d7285a17f1482ba2e331870
MayurSMahajan <mayur23smahajan@gmail.com>
2023-08-28 19:14:49 +0530

    chore: authentication repo base

---
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -0,0 +1,4 @@
+/// package which manages the authentication domain.
+library authentication_repository;
+
+export 'src/authentication_repository.dart';
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// package which manages the authentication domain.
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
