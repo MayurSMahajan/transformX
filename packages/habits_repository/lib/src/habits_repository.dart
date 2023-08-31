9607e6f7ac4c539610ab27040acbc97728fba7ef
MayurSMahajan <mayur23smahajan@gmail.com>
2023-08-31 17:26:42 +0530

    feat: habits repository

--- packages/habits_repository/lib/src/habits_repository.dart
+++ packages/habits_repository/lib/src/habits_repository.dart
@@ -1,7 +1,27 @@
+import 'package:habits_api/habits_api.dart';
+
 /// {@template habits_repository}
-/// A repository that handles habits related requests.
+/// A repository that handles `Habit` related requests.
 /// {@endtemplate}
 class HabitsRepository {
   /// {@macro habits_repository}
-  const HabitsRepository();
+  const HabitsRepository({
+    required HabitsApi habitsApi,
+  }) : _habitsApi = habitsApi;
+
+  final HabitsApi _habitsApi;
+
+  /// Provides a [Stream] of all habits.
+  Stream<List<Habit>> getHabits() => _habitsApi.getHabits();
+
+  /// Saves a [Habit].
+  ///
+  /// If a [Habit] with the same id already exists, it will be replaced.
+  Future<void> saveHabit(Habit habit) => _habitsApi.saveHabit(habit);
+
+  /// Deletes the `Habit` with the given id.
+  ///
+  /// If no `Habit` with the given id exists, a [HabitNotFoundException] error is
+  /// thrown.
+  Future<void> deleteHabit(String id) => _habitsApi.deleteHabit(id);
 }
--- packages/habits_repository/test/src/habits_repository_test.dart
+++ packages/habits_repository/test/src/habits_repository_test.dart
@@ -1,11 +1,125 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:habits_api/habits_api.dart';
 import 'package:habits_repository/habits_repository.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+
+class MockHabitsApi extends Mock implements HabitsApi {}
+
+class FakeHabit extends Fake implements Habit {}
 
 void main() {
   group('HabitsRepository', () {
-    test('can be instantiated', () {
-      expect(HabitsRepository(), isNotNull);
+    late HabitsApi api;
+
+    final habits = [
+      Habit(
+        id: '1',
+        title: 'title 1',
+        location: 'location 1',
+        metric: Metric(title: 'metric title 1', minimum: 0, ideal: 100),
+        time: Time(hour: 1, mins: 2),
+        ritual: 'ritual 1',
+        shortReward: 'shortReward 1',
+        longReward: 'longReward 1',
+        icon: 'icon 1,',
+      ),
+      Habit(
+        id: '2',
+        title: 'title 2',
+        location: 'location 2',
+        metric: Metric(title: 'metric title 2', minimum: 0, ideal: 200),
+        time: Time(hour: 2, mins: 2),
+        ritual: 'ritual 2',
+        shortReward: 'shortReward 2',
+        longReward: 'longReward 2',
+        icon: 'icon 2,',
+      ),
+      Habit(
+        id: '3',
+        title: 'title 3',
+        location: 'location 3',
+        metric: Metric(title: 'metric title 3', minimum: 0, ideal: 300),
+        time: Time(hour: 3, mins: 3),
+        ritual: 'ritual 3',
+        shortReward: 'shortReward 3',
+        longReward: 'longReward 3',
+        icon: 'icon 3,',
+      ),
+    ];
+
+    setUpAll(() {
+      registerFallbackValue(FakeHabit());
+    });
+
+    setUp(() {
+      api = MockHabitsApi();
+      when(() => api.getHabits()).thenAnswer((_) => Stream.value(habits));
+      when(() => api.saveHabit(any())).thenAnswer((_) async {});
+      when(() => api.deleteHabit(any())).thenAnswer((_) async {});
+    });
+
+    HabitsRepository createSubject() => HabitsRepository(habitsApi: api);
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(
+          createSubject,
+          returnsNormally,
+        );
+      });
+    });
+
+    group('getHabits', () {
+      test('makes correct api request', () {
+        final subject = createSubject();
+
+        expect(
+          subject.getHabits(),
+          isNot(throwsA(anything)),
+        );
+
+        verify(() => api.getHabits()).called(1);
+      });
+
+      test('returns stream of current list habits', () {
+        expect(
+          createSubject().getHabits(),
+          emits(habits),
+        );
+      });
+    });
+
+    group('saveHabit', () {
+      test('makes correct api request', () {
+        final newHabit = Habit(
+          id: '4',
+          title: 'title 3',
+          location: 'location 3',
+          metric: Metric(title: 'metric title 3', minimum: 0, ideal: 300),
+          time: Time(hour: 3, mins: 3),
+          ritual: 'ritual 3',
+          shortReward: 'shortReward 3',
+          longReward: 'longReward 3',
+          icon: 'icon 3,',
+        );
+
+        final subject = createSubject();
+
+        expect(subject.saveHabit(newHabit), completes);
+
+        verify(() => api.saveHabit(newHabit)).called(1);
+      });
+    });
+
+    group('deleteHabit', () {
+      test('makes correct api request', () {
+        final subject = createSubject();
+
+        expect(subject.deleteHabit(habits[0].id), completes);
+
+        verify(() => api.deleteHabit(habits[0].id)).called(1);
+      });
     });
   });
 }
