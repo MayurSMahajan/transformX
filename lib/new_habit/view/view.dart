aa278923affa790fe5831c6fcdceb03aadd1eaff
MayurSMahajan <mayur23smahajan@gmail.com>
2023-09-08 18:02:02 +0530

    refactor: separate habit views

--- lib/new_habit/view/view.dart
+++ lib/new_habit/view/view.dart
@@ -1,1 +1,6 @@
+export 'habit_complete_form.dart';
+export 'habit_half_form.dart';
+export 'habit_initial_form.dart';
+export 'habit_quarter_and_half_form.dart';
+export 'habit_quarter_form.dart';
 export 'new_habit_page.dart';
